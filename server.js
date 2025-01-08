a8bc552eeb42e491b2555db906121c84d1cf9e0c
Keffa Kirwa <s223664997@deakin.edu.au>
2025-01-09 07:23:48 +1100

    added unit testing

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 const express = require("express");
-const  nunjucks = require("nunjucks")
-const catRoutes = require('./routes/cats.routes.js')
-const indexRoutes = require('./routes/index.routes.js')
+const nunjucks = require("nunjucks");
+const catRoutes = require("./routes/cats.routes.js");
+const indexRoutes = require("./routes/index.routes.js");
 
 const app = express();
 
-nunjucks.configure("views",{
-  autoescape:true,
-  express:app,
-})
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/addTwoNumbers/:firstNumber/:secondNumber", function (req, res, next) {
+  var firstNumber = parseInt(req.params.firstNumber);
+  var secondNumber = parseInt(req.params.secondNumber);
+  var result = firstNumber + secondNumber || null;
+  if (result == null) {
+    res.json({ result: result, statusCode: 400 }).status(400);
+  } else {
+    res.json({ result: result, statusCode: 200 }).status(200);
+  }
+});
 
-app.use('/',indexRoutes)
-app.use("/api/cats",catRoutes)
-
+app.use("/", indexRoutes);
+app.use("/api/cats", catRoutes);
 
 const port = process.env.PORT || 3000;
 
